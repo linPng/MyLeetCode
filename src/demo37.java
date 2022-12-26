f5aabd08df46163b1e5db9c373e645fc0adf3094
linpeng <645558264@qq.com>
2022-12-26 19:25:38 +0800

    1

--- src/demo37.java
+++ src/demo37.java
@@ -1,3 +1,4 @@
+import javax.lang.model.SourceVersion;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,6 +20,7 @@ public class demo37 {
         }
         while(true){
             boolean flag=true;
+            boolean noupdate=true;
             for(int i=0;i<9;i++){
                 for(int j=0;j<9;j++){
                     char e=board[i][j];
@@ -35,19 +37,78 @@ public class demo37 {
                             h[i].add(t);
                             s[j].add(t);
                             f[(i/3)*3+j/3].add(t);
+                            noupdate=false;
                         }else if(b.size()>1){
                             flag=false;
                         }
                         System.out.println("e = " + e);
                     }else{
-                        h[i].add(n);
+                        noupdate=h[i].add(n);
                         s[j].add(n);
                         f[(i/3)*3+j/3].add(n);
                     }
                 }
             }
             if(flag)return;
+            if(noupdate)break;
         }
+
+        fillNum(board,h,s,f);
+
+        System.out.println("1111 = " + 1111);
+
+    }
+
+    public static boolean fillNum(char[][] board,Set[] h,Set[] s,Set[] f) {
+        boolean isend =true;
+        Set<Integer> a=new HashSet<>();
+        for(int i=0;i<9;i++){
+            a.add(i);
+            if(h[i].size()!=9||s[i].size()!=9||f[i].size()!=9){
+                isend = false;
+            }
+        }
+        if(isend){
+            return true;
+        }
+        for(int x = 0; x < 9; x++) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    char e = board[i][j];
+                    int n = e - '0';
+                    if (e == '.') {
+                        Set<Integer> b = new HashSet<>(a);
+                        b.removeAll(h[i]);
+                        b.removeAll(s[j]);
+                        b.removeAll(f[(i / 3) * 3 + j / 3]);
+                        if (b.size() <= 0) {
+                            return false;
+                        }
+                        for(int y = 0; y < x; y++) {
+                            b.iterator().next();
+                        }
+                        int t = b.iterator().next();
+                        System.out.println("t = " + t);
+                        board[i][j] = Character.valueOf(String.valueOf(t).charAt(0));
+                        h[i].add(t);
+                        s[j].add(t);
+                        f[(i / 3) * 3 + j / 3].add(t);
+                        boolean callback = fillNum(board, h, s, f);
+                        if (!callback) {
+                            System.out.println("remove t = " + t);
+                            System.out.println("||||| x = " + x+";i = " + i+";j = " + j);
+                            board[i][j] = '.';
+                            h[i].remove(t);
+                            s[j].remove(t);
+                            f[(i / 3) * 3 + j / 3].remove(t);
+                        }else{
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
