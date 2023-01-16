c00d7c4a3c0fb31578fcc19bb20cb04b24e5aeff
linpeng <645558264@qq.com>
2023-01-16 10:16:28 +0800

    1

--- src/demo146.java
+++ src/demo146.java
@@ -4,25 +4,112 @@ import java.util.List;
 import java.util.Map;
 
 public class demo146 {
-    //LRU 缓存  LinkHashMap 数组+红黑树+链表
-    class LRUCache {
-        int maxsize=0;
-        Map<Integer,Integer> map;
-        //List<Integer,Integer> list=new
+    //LRU 缓存  LinkHashMap 数组+红黑树+链表 不会
+    //哈希表 双向链表    哈希表：键值对；双向链表：随机访问，写入头尾
+    public class LRUCache {
+        class DLinkedNode {
+            int key;
+            int value;
+            DLinkedNode prev;
+            DLinkedNode next;
+            public DLinkedNode() {}
+            public DLinkedNode(int _key, int _value) {key = _key; value = _value;}
+        }
+
+        private Map<Integer, DLinkedNode> cache = new HashMap<Integer, DLinkedNode>();
+        private int size;
+        private int capacity;
+        private DLinkedNode head, tail;
+
         public LRUCache(int capacity) {
-            maxsize=capacity;
-            map = new LinkedHashMap<>();
+            this.size = 0;
+            this.capacity = capacity;
+            // 使用伪头部和伪尾部节点
+            head = new DLinkedNode();
+            tail = new DLinkedNode();
+            head.next = tail;
+            tail.prev = head;
         }
 
         public int get(int key) {
-            return map.get(Integer.valueOf(key));
+            DLinkedNode node = cache.get(key);
+            if (node == null) {
+                return -1;
+            }
+            // 如果 key 存在，先通过哈希表定位，再移到头部
+            moveToHead(node);
+            return node.value;
         }
 
         public void put(int key, int value) {
-            map.put(key,value);
-            if(map.size()>maxsize){
-                //map.();
+            DLinkedNode node = cache.get(key);
+            if (node == null) {
+                // 如果 key 不存在，创建一个新的节点
+                DLinkedNode newNode = new DLinkedNode(key, value);
+                // 添加进哈希表
+                cache.put(key, newNode);
+                // 添加至双向链表的头部
+                addToHead(newNode);
+                ++size;
+                if (size > capacity) {
+                    // 如果超出容量，删除双向链表的尾部节点
+                    DLinkedNode tail = removeTail();
+                    // 删除哈希表中对应的项
+                    cache.remove(tail.key);
+                    --size;
+                }
             }
+            else {
+                // 如果 key 存在，先通过哈希表定位，再修改 value，并移到头部
+                node.value = value;
+                moveToHead(node);
+            }
+        }
+
+        private void addToHead(DLinkedNode node) {
+            node.prev = head;
+            node.next = head.next;
+            head.next.prev = node;
+            head.next = node;
+        }
+
+        private void removeNode(DLinkedNode node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+
+        private void moveToHead(DLinkedNode node) {
+            removeNode(node);
+            addToHead(node);
+        }
+
+        private DLinkedNode removeTail() {
+            DLinkedNode res = tail.prev;
+            removeNode(res);
+            return res;
+        }
+    }
+
+    //LinkHashMap LinkedHashMap结合了hashmap和双向链表
+    class LRUCache2 extends LinkedHashMap{
+        int maxsize=0;
+        //List<Integer,Integer> list=new
+        public LRUCache2(int capacity) {
+            super(capacity,0.75F,true);
+            maxsize=capacity;
+            //如果accessOrder为true的话，则会把访问过的元素放在链表后面  默认false按插入顺序排序
+        }
+
+        public int get(int key) {
+            return (int)super.getOrDefault(Integer.valueOf(key),-1);
+        }
+
+        public void put(int key, int value) {
+            super.put(key,value);
+        }
+        @Override
+        protected boolean removeEldestEntry(Map.Entry eldest) {
+            return super.size()>maxsize;
         }
     }
 }
