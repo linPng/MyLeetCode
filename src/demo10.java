016e0d707c6baefabd369242fbcbe9e211f5cb5e
linpeng <645558264@qq.com>
2023-01-18 18:45:36 +0800

    1

--- src/demo10.java
+++ src/demo10.java
@@ -6,20 +6,20 @@ public class demo10 {
         char[] cp=p.toCharArray();
         boolean[][] dp=new boolean[s.length()+1][p.length()+1];//自带空字符串
         dp[0][0]=true;
-        for(int j=1;j<=p.length();j++){
-            if(cp[j-1]=='*'){
-                dp[0][j]=dp[0][j-2];
+        for(int j=0;j<p.length();j++){
+            if(cp[j]=='*'){
+                dp[0][j+1]=dp[0][j-1];//*可以不匹配
             }
 
         }
         for(int i=1;i<=s.length();i++){
             for(int j=1;j<=p.length();j++){
-                if(cp[j-1]==cs[i-1]||cp[j-1]=='.'){
+                if(cp[j-1]==cs[i-1]||cp[j-1]=='.'){//匹配
                     dp[i][j]=dp[i-1][j-1];
                 }
                 if(cp[j-1]=='*'&&(cp[j-2]==cs[i-1]||cp[j-2]=='.')){
                     dp[i][j]=dp[i][j-2]||dp[i-1][j-1]||dp[i-1][j];
-                }else if(cp[j-1]=='*'){
+                }else if(cp[j-1]=='*'){//*可以不匹配
                     dp[i][j]=dp[i][j-2];
                 }
             }
