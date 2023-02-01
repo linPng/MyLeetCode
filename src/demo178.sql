4c1e3593f9332fa31700268a4bc2fdebf24cc5ed
linpeng <645558264@qq.com>
2023-02-01 19:15:52 +0800

    1

--- src/demo178.sql
+++ src/demo178.sql
@@ -1,5 +1,8 @@
 <!--第二高的薪水 简单 -->
-select a.Score as Score,
-(select count(distinct b.Score) from Scores b where b.Score >= a.Score) as Rank
-from Scores a
-order by a.Score DESC
+select Score,
+dense_rank() OVER (
+        ORDER BY Score DESC
+    ) as "rank"
+from Scores
+order by Score DESC
+<!-- dense_rank 和 rank 不同的地方在与dense不会跳过排名  如1，1，2，3 而不是 1，1，3，4； -->
