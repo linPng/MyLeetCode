9c77a52fe8f4418b298981b2c9e451fd8ce83f0d
linpeng <645558264@qq.com>
2023-02-01 19:55:44 +0800

    1

--- src/demo198.java
+++ src/demo198.java
@@ -2,8 +2,13 @@ public class demo198 {
     //打家劫舍 中等
     //动态规划
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        dp[0]=nums[0];
+        int[] dp = new int[nums.length+1];
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<nums.length+1;i++){
+            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp[nums.length];
     }
     public static void main(String[] args) {
 
