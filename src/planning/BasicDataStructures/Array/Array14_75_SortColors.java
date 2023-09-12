0cad2ac8ae465b4e08fdc5b502153e5b4c7b9434
linpeng <645558264@qq.com>
2023-09-12 09:46:06 +0800

    1

--- src/planning/BasicDataStructures/Array/Array14_75_SortColors.java
+++ src/planning/BasicDataStructures/Array/Array14_75_SortColors.java
@@ -3,7 +3,25 @@ package planning.BasicDataStructures.Array;
 public class Array14_75_SortColors {
 
 
-
+    public void sortColors(int[] nums) {
+        int a=0,b=0;
+        for(int i:nums){
+            if(i==0){
+                a++;
+            }else if(i==1){
+                b++;
+            }
+        }
+        for(int i=0;i<a;i++){
+            nums[i]=0;
+        }
+        for(int i=a;i<a+b;i++){
+            nums[i]=1;
+        }
+        for(int i=a+b;i<nums.length;i++){
+            nums[i]=2;
+        }
+    }
 
 
 }
