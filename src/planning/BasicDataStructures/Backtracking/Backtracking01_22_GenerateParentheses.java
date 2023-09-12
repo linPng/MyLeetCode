d7e1b81e14b083c2b3341eb916c0e2f882a5816e
linpeng <645558264@qq.com>
2023-09-12 11:39:31 +0800

    1

--- src/planning/BasicDataStructures/Backtracking/Backtracking01_22_GenerateParentheses.java
+++ src/planning/BasicDataStructures/Backtracking/Backtracking01_22_GenerateParentheses.java
@@ -1,9 +1,29 @@
 package planning.BasicDataStructures.Backtracking;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Backtracking01_22_GenerateParentheses {
 
 
-
+    public List<String> generateParenthesis(int n) {
+        if(n==1){
+            List<String> ret = new ArrayList<>();
+            ret.add("()");
+            return ret;
+        }
+        List<String> pre=generateParenthesis(n-1);
+        Set<String> set=new HashSet<>();
+        for(String s:pre){
+            for(int i=0;i<=s.length();i++){
+                set.add(s.substring(0,i)+"()"+s.substring(i));
+            }
+        }
+        List<String> ret = new ArrayList<>(set);
+        return ret;
+    }
 
 
 }
--- src/planning/BasicDataStructures/Backtracking/Backtracking02_78_Subsets.java
+++ src/planning/BasicDataStructures/Backtracking/Backtracking02_78_Subsets.java
@@ -1,9 +1,43 @@
 package planning.BasicDataStructures.Backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Backtracking02_78_Subsets {
 
 
+    //二进制
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<>();
+        int n=nums.length;
+        int k=1<<n;
+        for(int i=0;i<k;i++){
+            List<Integer> t = new ArrayList<Integer>();
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    t.add(nums[j]);
+                }
+            }
+            ret.add(new ArrayList<>(t));
+        }
+        return ret;
+    }
 
-
+    //回溯
+    List<List<Integer>> retu = new ArrayList<>();
+    public List<List<Integer>> subsets2(int[] nums) {
+        dfs(nums,new ArrayList<>(),0);
+        return retu;
+    }
+    public void dfs(int[] nums,List<Integer> d,int k){
+        if(k==nums.length){
+            retu.add(new ArrayList<>(d));
+            return;
+        }
+        d.add(nums[k]);
+        dfs(nums,d,k+1);
+        d.remove(d.size()-1);
+        dfs(nums,d,k+1);
+    }
 
 }
