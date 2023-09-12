fd04ef572489ea400cdc230b4ed8d0346f326fa4
linpeng <645558264@qq.com>
2023-09-12 09:47:15 +0800

    1

--- src/planning/BasicDataStructures/Array/Array15_57_InsertInterval.java
+++ src/planning/BasicDataStructures/Array/Array15_57_InsertInterval.java
@@ -1,9 +1,32 @@
 package planning.BasicDataStructures.Array;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class Array15_57_InsertInterval {
-
-
-
-
-
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        PriorityQueue<int[]> li = new PriorityQueue<int[]>((a,b)->a[0]-b[0]);
+        int n= intervals.length;
+        for(int i=0;i<n;i++){
+            int[] t=intervals[i];
+            if(t[1]<newInterval[0]||t[0]>newInterval[1]){
+                li.offer(t);
+                //System.out.println("t[0] = " + t[0]+"t[1] = " + t[1]);
+            }else{
+                newInterval[0]=Math.min(newInterval[0],t[0]);
+                newInterval[1]=Math.max(newInterval[1],t[1]);
+            }
+        }
+        li.offer(newInterval);
+        //System.out.println("newInterval[0] = " + newInterval[0]+"newInterval[1] = " + newInterval[1]);
+        //System.out.println("li.size() = " + li.size());
+        int rn=li.size();
+        int[][] ret=new int[rn][];
+        for(int i=0;i<rn;i++){
+            ret[i]=li.poll();
+            //System.out.println("ret[i] = " + ret[i]);
+        }
+        return ret;
+    }
 }
--- src/planning/BasicDataStructures/Array/Array16_42_TrappingRainWater.java
+++ src/planning/BasicDataStructures/Array/Array16_42_TrappingRainWater.java
@@ -2,7 +2,22 @@ package planning.BasicDataStructures.Array;
 
 public class Array16_42_TrappingRainWater {
 
-
+    public int trap(int[] height) {
+        int ret=0;
+        int n=height.length;
+        int[] dp=new int[n];
+        int m=0;
+        for(int i=0;i<n;i++){
+            dp[i]=m;
+            m=Math.max(m,height[i]);
+        }
+        m=0;
+        for(int i=n-1;i>=0;i--){
+            ret+=Math.max(Math.min(dp[i],m)-height[i],0);
+            m=Math.max(m,height[i]);
+        }
+        return ret;
+    }
 
 
 
