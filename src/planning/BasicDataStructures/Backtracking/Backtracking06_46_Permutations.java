c447ed176d1daf636e58920802d73f1ae138f906
linpeng <645558264@qq.com>
2023-09-14 12:41:41 +0800

    1

--- src/planning/BasicDataStructures/Backtracking/Backtracking06_46_Permutations.java
+++ src/planning/BasicDataStructures/Backtracking/Backtracking06_46_Permutations.java
@@ -1,9 +1,30 @@
 package planning.BasicDataStructures.Backtracking;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Backtracking06_46_Permutations {
 
 
-
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ret= new LinkedList<>();
+        if(nums.length==1){
+            List<Integer> t = new LinkedList<>();
+            t.add(nums[0]);
+            ret.add(t);
+            return ret;
+        }
+        List<List<Integer>> per = permute(Arrays.copyOfRange(nums, 0, nums.length - 1));
+        for(List<Integer> li:per){
+            for(int i=0;i<=li.size();i++){
+                List<Integer> t = new LinkedList<>(li);
+                t.add(i,nums[nums.length-1]);
+                ret.add(t);
+            }
+        }
+        return ret;
+    }
 
 
 }
