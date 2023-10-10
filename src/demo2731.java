aba9d9f3689e74050b132937e315294c5b80c2c8
linpeng <645558264@qq.com>
2023-10-10 14:04:20 +0800

    1

---
+++ src/demo2731.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class demo2731 {
+    //2731.移动机器人
+    static final int MOD = 1000000007;
+
+    public int sumDistance(int[] nums, String s, int d) {
+        int n = nums.length;
+        long[] pos = new long[n];
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'L') {
+                pos[i] = (long) nums[i] - d;
+            } else {
+                pos[i] = (long) nums[i] + d;
+            }
+        }
+        Arrays.sort(pos);
+        //计算距离和 (pos[i]−pos[i−1])×i×(n−i)
+        long res = 0;
+        for (int i = 1; i < n; i++) {
+            res += 1L * (pos[i] - pos[i - 1]) * i % MOD * (n - i) % MOD;
+            res %= MOD;
+        }
+        return (int) res;
+    }
+    public static void main(String[] args) {
+
+    }
+}
